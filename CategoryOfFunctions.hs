d24f6174426e0de3d3f6aef51cf44514067c2003
unknown <richardsouthwell254@gmail.com>
2020-06-20 00:31:52 +0100

    added arrow composition

--- CategoryOfFunctions.hs
+++ CategoryOfFunctions.hs
@@ -257,3 +257,11 @@ myVertices2 (sa1, ma, sa0) (sb1, mb, sb0) = Data.Set.toList (t3 dataa) where dat
 -- Subobject classifier
 -- chi, and "subobject classified by arrow"
 -- identity arrows
+
+-- carr works as "after", for arrow composition
+carr :: Ord a1 => Ord a0 => Ord b1 => Ord b0 => Ord c1 => Ord c0 => (Map b1 c1, Map b0 c0) -> (Map a1 b1, Map a0 b0) -> (Map a1 c1, Map a0 c0)
+carr (g1, g0) (f1, f0) = (Data.Map.map (g1 !) f1, Data.Map.map (g0 !) f0)
+
+--map3 = Data.Map.fromList [("a",2), ("b",3)]
+--map4 = Data.Map.fromList [(2,3), (3,7)]
+--map5 = Data.Map.map (map4 !) map3
